ba9324b073d7995fcb98eb636821970381d3b925
jebbaited <kostadrapak@gmail.com>
2022-05-18 14:23:30 +0300

    added everything else

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ConversionComponent } from './conversion/conversion.component';
+import { SelectComponent } from './UI/select/select.component';
+import { MyInputComponent } from './UI/my-input/my-input.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConversionComponent,
+    SelectComponent,
+    MyInputComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
